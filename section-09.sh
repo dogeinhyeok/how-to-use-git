917df3dad7505e8890863579260666b11763d408
dogeinhyeok <devinhyeok@gmail.com>
2025-06-17 13:01:59 +0900

    add section-09.sh

---
+++ section-09.sh
@@ -0,0 +1,23 @@
+# 깃허브 저장소 클론할 디렉토리로 이동
+cd ~/how-to-use-git
+
+# 깃허브 저장소 클론
+git clone https://github.com/dogeinhyeok/how-to-use-git.git
+
+# 소스 확인
+ls -a
+
+#
+git remote add origin git@github.com:dogeinhyeok/how-to-use-git.git
+
+#
+git remote
+
+# 
+git remote -v
+
+#
+git push -u origin main
+
+#
+git add .
